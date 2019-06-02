0e377cd682afb9a33d1583a423b07be8ce232eac
jackee777 <stardustchronicle10030709@gmail.com>
2019-06-02 11:57:21 +0900

    Update check_ms2vec.py

--- check_program/check_ms2vec.py
+++ check_program/check_ms2vec.py
@@ -1,10 +1,12 @@
 from gensim.models.word2vec import LineSentence
 from ms2vec.ms2vec import MultiSense2Vec
 
-corpus = LineSentence("test")
-model = MultiSense2Vec(corpus, sg=1, negative=5, workers=1, iter=1,
-                       min_count=3, min_sense_count=10, max_sense_num=3)
+corpus = LineSentence("../enwiki_cleaning.txt")
+model = MultiSense2Vec(corpus, sg=1, negative=1, workers=4, iter=5,
+                       min_count=10, min_sense_count=4000, max_sense_num=2)
 
 #print(model.wv.index2word)
 print(model.most_similar("mouse"))
-model.save("mssg_test")
+model_name = "mssg_enwiki_sense_2_neg_1_edition2"
+model.save(model_name)
+model.wv.save_word2vec_format(model_name+".bin", binary=True)
