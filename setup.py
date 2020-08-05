39c37a7c32adc38c8d3820740b6212be08ad5817
jackee777_lab <d5620003@aoyama.jp>
2020-08-04 23:03:08 -0400

    revise setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import numpy as np
 
 setup(
     name="ms2vec",
+    packages=["ms2vec"],
     version="0.1.0",
     ext_modules=cythonize("./ms2vec/ms2vec_inner.pyx", compiler_directives={'language_level': 3}),
     include_dirs=[np.get_include()],
